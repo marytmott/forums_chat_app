7a572e4cd8d266c0d410f15f2bd980af4d83bc62
Mary Mott <mott.maryt@gmail.com>
2015-09-22 22:33:08 -0700

    progress on forum routes and debugging

--- controllers/forums.js
+++ controllers/forums.js
@@ -1,6 +1,8 @@
 var db = require('../models');
 var routeMiddleware = require('../middleware/routeHelpers');
 
+//ensure logged in everywhere!
+
 app.get('/forums', routeMiddleware.ensureLoggedIn, function(req, res) {
   db.Forum.find({}, function(err, forums) {
     if (err) {
@@ -28,12 +30,42 @@ app.get('/forums/new', routeMiddleware.ensureLoggedIn, function(req, res) {
   res.render('forums/new_forum', {docTitle: 'Create New Forum'});
 });
 
-app.get('/forums/:forum_name', function(req, res) {
+app.get('/forums/:forum_name', routeMiddleware.ensureLoggedIn, function(req, res) {
   db.Forum.findOne({name: req.params.forum_name}).populate('posts').exec(function(err, forum) {
     if (err) {
       console.log(err);
     } else {
+      // console.log(forum);
       res.render('forums/forum', {docTitle: forum.name, forum: forum});
     }
    });
+});
+
+app.put('/forums/:forum_name', routeMiddleware.ensureLoggedIn, function(req, res) {
+
+  console.log(req.body.forum);
+  if (req.body.forum) {
+    db.Forum.update({name: req.params.forum_name}, req.body.forum, function(err, forum) {
+      // console.log(forum);
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect('/forums/' + forum.name);
+      }
+    });
+  } else {
+    res.redirect('/forums/' + req.params.forum_name);
+  }
+});
+
+// app.delete();
+
+app.get('/forums/:forum_name/modify', routeMiddleware.ensureLoggedIn, function(req, res) {
+  db.Forum.findOne({name: req.params.forum_name}, function(err, forum) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('forums/modify_forum', {docTitle: 'Modify' + forum.name, forum: forum});
+    }
+  });
 });
