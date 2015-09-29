9411c72fddc859311e922c9efd3b1c2bfc75fa68
Mary Mott <mott.maryt@gmail.com>
2015-09-29 09:24:41 -0700

    updated styles

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,7 +2,7 @@ $(function() {
 
   function openChat(e) {
     e.preventDefault();
-    var windowObjectReference = window.open('/chat', 'Forum Chat', 'width=500, height=600, resizable=yes');
+    var windowObjectReference = window.open('/chat', 'Forum Chat', 'width=550, height=550, resizable=yes');
   }
 
   $('a[href="/chat"').on('click', openChat);
